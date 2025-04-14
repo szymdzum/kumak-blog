8c445ecbcd28747a7bdcb5bce9683276cae3e2da
Kumak <szymon@kumak.dev>
2025-04-15 01:31:09 +0200

    update: modified and deleted files after status check

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import deno from '@astrojs/deno';
+import { rehypePrettyCode } from 'rehype-pretty-code';
+import { transformerCopyButton } from '@rehype-pretty/transformers';
+import moonlightTheme from './public/theme/moonlight-ii.json';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +13,38 @@ export default defineConfig({
   output: 'server',
   adapter: deno(),
   integrations: [mdx(), sitemap()],
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: moonlightTheme,
+          keepBackground: true,
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode
+            if (node.children.length === 0) {
+              node.children = [{ type: 'text', value: ' ' }];
+            }
+          },
+          onVisitHighlightedLine(node) {
+            // Add highlighting styles
+            node.properties.className.push('highlighted-line');
+          },
+          onVisitHighlightedWord(node) {
+            // Add word highlighting styles
+            node.properties.className = ['highlighted-word'];
+          },
+          transformers: [
+            transformerCopyButton({
+              visibility: 'hover',
+              feedbackDuration: 2000,
+            }),
+          ],
+        },
+      ],
+    ],
+  },
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp',
