28036fac360e2606885744608dad057e854eff45
Kumak <szymon@kumak.dev>
2025-04-15 22:23:56 +0200

    type heavy edit

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,16 +2,13 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z as schema } from 'astro:content';
 
 const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-  // Type-check frontmatter using a schema
   schema: schema.object({
     title: schema.string(),
     description: schema.string(),
-    // Transform string to Date object
+    heroImage: schema.string().optional(),
     pubDate: schema.coerce.date(),
     updatedDate: schema.coerce.date().optional(),
-    heroImage: schema.string().optional(),
   }),
 });
 
